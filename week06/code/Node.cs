12c3f70899676ac5746687964c9a66c0b35c9b45
Jorge Menjivar <menjivar.jorge.93@gmail.com>
2025-08-09 01:55:22 -0600

    week 6 completed

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -12,6 +12,10 @@ public class Node
     public void Insert(int value)
     {
         // TODO Start Problem 1
+        if (value == Data)
+        {
+            return;
+        }
 
         if (value < Data)
         {
@@ -34,12 +38,29 @@ public class Node
     public bool Contains(int value)
     {
         // TODO Start Problem 2
-        return false;
+        if (value == Data)
+            return true;
+        else if (value < Data)
+            return Left != null && Left.Contains(value);
+        else
+            return Right != null && Right.Contains(value);
     }
 
     public int GetHeight()
     {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        int leftHeight = 0;
+        int rightHeight = 0;
+
+        if (Left != null)
+            leftHeight = Left.GetHeight();
+
+        if (Right != null)
+            rightHeight = Right.GetHeight();
+
+        if (leftHeight > rightHeight)
+            return 1 + leftHeight;
+        else
+            return 1 + rightHeight;
     }
 }
